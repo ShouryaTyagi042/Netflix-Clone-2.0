fee126992c18eebd3773f8c25c3ec1034c8203e6
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-07-26 00:08:54 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
 import HomeScreen from "./pages/HomeScreen";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <HomeScreen></HomeScreen>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeScreen />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
