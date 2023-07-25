0e63d2aa4fb57bdfdaed7fd6ab0efd72b80d8838
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-07-25 22:18:03 +0530

    added firebase config files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBAmWG99i_RbdVLWugc0M-GP8sj25hjh9E",
+  authDomain: "netflixclone-72cfb.firebaseapp.com",
+  projectId: "netflixclone-72cfb",
+  storageBucket: "netflixclone-72cfb.appspot.com",
+  messagingSenderId: "141405104483",
+  appId: "1:141405104483:web:b82e476736a3dd8b8acb56",
+};
