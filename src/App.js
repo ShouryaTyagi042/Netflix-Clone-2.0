5530efdf04495ed0ac3f288fe9f24062ede619c0
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-07-26 19:34:08 +0530

    Set up userSlice in redux

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import HomeScreen from "./pages/HomeScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from "./pages/LoginScreen";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        //Logged In
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
-        //logged Out
+        //Logged Out
+        dispatch(logout);
       }
     });
     return unsubscribe;
