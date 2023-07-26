77183e9989e04404dc315a54b32c7601fb039bb0
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-07-26 15:59:23 +0530

    Login Screen Background

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -7,6 +7,8 @@ export default function LoginScreen() {
     <div className="loginScreen">
       <div className="loginScreen_background ">
         <img src={logo} alt="loginScreen_logo" className="loginScreen_logo" />
+        <button className="loginScreen_button">Sign In</button>
+        <div className="loginScreen_gradient"></div>
       </div>
     </div>
   );
