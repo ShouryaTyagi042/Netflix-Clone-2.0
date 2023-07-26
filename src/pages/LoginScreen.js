7481f4f1a7cf330de2911e6f3c4d7e756e86dd0a
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-07-26 17:12:10 +0530

    Completed login screen

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -10,6 +10,20 @@ export default function LoginScreen() {
         <button className="loginScreen_button">Sign In</button>
         <div className="loginScreen_gradient"></div>
       </div>
+      <div className="loginScreen_body">
+        <h1>Unlimited films, TV programmes and more.</h1>
+        <h2>Watch anywhere. Cancel at any time.</h2>
+        <h3>
+          Ready to watch? Enter your email to create or restart your membership.
+          Email address. Get Started.{" "}
+        </h3>
+        <div className="loginScreen_input">
+          <form>
+            <input type="email" placeholder="Enter your email address"></input>
+            <button className="getStarted">Get Started</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
